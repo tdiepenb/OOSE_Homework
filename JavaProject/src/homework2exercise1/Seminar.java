18aaf2012b0f6933bb9fcc07f3b23c81f7bb1203
Tilmann Diepenbruck <tdiepenb@smail.uni-koeln.de>
2024-05-17 13:38:27 +0200

    resets to a typical generic implementation without the wildcard implementation. The wildcard implementation can be viewed on the branch homework2

--- JavaProject/src/homework2exercise1/Seminar.java
+++ JavaProject/src/homework2exercise1/Seminar.java
@@ -3,10 +3,12 @@ package homework2exercise1;
 import java.util.ArrayList;
 import java.util.List;
 
+// An implementation using wildcards that allows classes that extend IStudent to be added to participants
+// can be found on the branch homework2
 public class Seminar<T extends IStudent> {
     private String name;
     private String id;
-    private List<IStudent> participants;
+    private List<T> participants;
 
     //TODO: List of participants
 
@@ -17,26 +19,18 @@ public class Seminar<T extends IStudent> {
         this.participants = new ArrayList<>();
     }
 
-    public <E extends IStudent> void addParticipant(E participant) {
+    public void addParticipant(T participant) {
         //TODO: Add to list of participants
         participants.add(participant);
     }
 
-    public void addParticipants(List<? extends IStudent> newParticipants) {
-        for(IStudent participant : newParticipants) {
-            if (!participants.contains(participant)) {
-                participants.add(participant);
-            }
-        }
-    }
-
-    public List<IStudent> getParticipants() {
+    public List<T> getParticipants() {
         return participants;
     }
 
-    public List<IStudent> getParticipantsFromStudyProgram(String studyProgram) {
-        List<IStudent> filteredList;
-        List<IStudent> originalList = this.getParticipants();
+    public List<T> getParticipantsFromStudyProgram(String studyProgram) {
+        List<T> filteredList;
+        List<T> originalList = this.getParticipants();
 
         filteredList = originalList.stream().filter(participant -> participant.getSubject().equals(studyProgram)).toList();
 
@@ -50,7 +44,6 @@ public class Seminar<T extends IStudent> {
         phdStudents.addParticipant(new PhD("Alice", "Software Engineering"));
         phdStudents.addParticipant(new PhD("Bob", "Algorithmics"));
         phdStudents.addParticipant(new PhD("Charly", "Visual Analytics"));
-        phdStudents.addParticipant(new Student("Zedrick", "Software Engineering"));
 
         Seminar<Student> bachelorStudents = new Seminar<Student>("Seminar Software Engineering", "SSE");
         bachelorStudents.addParticipant(new Student("Zedrick", "Wirtschaftsinformatik"));
@@ -58,8 +51,6 @@ public class Seminar<T extends IStudent> {
         bachelorStudents.addParticipant(new Student("Wladislav", "Mathematik"));
         bachelorStudents.addParticipant(new Student("Xenia", "Informatik"));
         bachelorStudents.addParticipant(new Student("Udo", "Informatik"));
-        bachelorStudents.addParticipant(new PhD("Adrian", "Informatik"));
-        bachelorStudents.addParticipants(phdStudents.getParticipants());
 
 
         System.out.println(phdStudents.getParticipantsFromStudyProgram("Software Engineering"));
@@ -75,7 +66,6 @@ public class Seminar<T extends IStudent> {
         System.out.println();
         System.out.println(bachelorStudents.getParticipantsFromStudyProgram("Informatik"));
         System.out.println();
-        System.out.println(bachelorStudents.getParticipantsFromStudyProgram("Software Engineering"));
-        System.out.println();
+
     }
 }
